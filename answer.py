d7066db70cc7c45315e9fed6489139d4559ed539
garameki <gomokumeshinomoto@gmail.com>
2018-12-21 02:43:49 +0900

    Add しすてむ 	modified:   answer.py 	modified:   google.txt 	modified:   google.voca

--- answer.py
+++ answer.py
@@ -10,9 +10,16 @@ import math
 import max31856
 max = max31856.Max31856(0,0)
 
+func_ONDO=1
+func_JIKOKU=2
+func_HIDUKE=3
+func_IJIN=4
+func_SYS=5
+
 phrases = [
-	[["偉人の言葉"],"はい、",4],
-	[["日付"],"えーと、",3],
+	[["システム"],"わかりました。",func_SYS],
+	[["偉人の言葉"],"はい、",func_IJIN],
+	[["日付"],"えーと、",func_HIDUKE],
 	[["呼んでない"],"すみません",0],
 	[["用ない"],"そうですか",0],
 	[["わかった"],"どういたしまして",0],
@@ -23,9 +30,12 @@ phrases = [
 	[["OK","仮面ライダー"],"わたしは、仮面ライダーではありません。",0],
 	[["OK","ワングル"],"わんわん",0],
 	[["くだらない"],"くだらなくて、申し訳ありません。",0],
-	[["温度"],"少々お待ちください",1],
-	[["時刻"],"えーと、",2],
+	[["温度"],"少々お待ちください",func_ONDO],
+	[["時刻"],"えーと、",func_JIKOKU],
+	[["面白い"],"そうですね。面白いですね。",0],
+	[["寒い"],"そうですね。寒いですね。",0],
 ]
+
 
 def conversation(words):
 	global phrases
@@ -55,19 +65,21 @@ def answer(words):
 		print('{0}'.format(phrase))
 		os.system(cmd)
 
+		command = None
+
 		if num == 0:
 			pass
 		else:
-			if num == 1:
+			if num == func_ONDO:
 				aTemps = max.read()
 				sTemp = str(math.floor(aTemps["HJ"]*0.0625+0.0001))
 				phrase = "ただいまの部屋の温度は　　"+sTemp+"度です。"
-			elif num == 2:
+			elif num == func_JIKOKU:
 				now = datetime.now()
 				hh = now.strftime("%H")
 				mm = now.strftime("%M")
 				phrase = "ただいまの時刻は   "+hh+"時"+mm+"分です。"
-			elif num == 3:
+			elif num == func_HIDUKE:
 				now = datetime.now()
 				MM = now.strftime("%m")
 				DD = now.strftime("%d")
@@ -107,16 +119,22 @@ def answer(words):
 					DD = DD+"にち"
 
 				phrase = "今日は   "+MM+"がつ"+DD+"　"+dd+"曜日です。"
-			elif num == 4:
+			elif num == func_IJIN:
 				nn = random.randint(0,4)
 				if nn == 0:phrase = "　　　　われ思う、　　ゆえに、　　われあり。　　　　　　　パスカル"
 				elif nn == 1:phrase = "　　　　小さな成功の積み重ねだけが、大成功に導いてくれる"
 				elif nn == 2:phrase = "　　　　自分を追い込んで、成果を残す。自分にプレッシャーを掛けなさい"
 				elif nn == 3:phrase = "　　　　午前中の数分のトレーニングが脳を若返らせる。トレーニングを続けることで、老いた脳から若い脳へ。"
 				elif nn == 4:phrase = "　　　　脳にとっての幸福は、学び続け、成長し続け、達成を繰り返すこと。"
+			elif num == func_SYS:
+				phrase = "ステータスをLCDに表示します。"
+				command = "/usr/bin/python3 /home/pi/src/i2c/acm1602/yoko_scroll.py &"
 			cmd = '/home/pi/src/aquestalkpi/AquesTalkPi -s 100 "　　　{0}" | aplay'.format(phrase)
 			print('{0}'.format(phrase))
 			os.system(cmd)
+
+			if command:
+				os.system(command)
 
 		return True
 ttBefore = ""
